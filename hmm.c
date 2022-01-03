06ec59dec61a473a540443b8b184f30244b3ae20
woalol <94733533+woalol@users.noreply.github.com>
2022-01-03 17:51:32 +0800

    hmm

---
+++ hmm.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+  int no = 0;
+  while (true)
+  {
+    int a = 34;
+    no++;
+    printf("We have printed %c hmm %c %i amount of times", a, a, no);
+  }
+}
